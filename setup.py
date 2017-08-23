9f1c95b812f13cd9f37da0de0de0260ac5b52e55
William Schoenell <wschoenell@gmail.com>
2017-08-23 14:57:44 -0300

    order matters?

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ setup(
     install_requires=['astropy',
                       'colorlog',
                       'ccdproc>=0.3.3',
+                      'pyyaml',
                       'fits_reduce',
-                      'pyyaml'],
+],
     dependency_links=['https://github.com/tribeiro/fits_reduce/archive/master.zip'],
     scripts=['scripts/t80s_check_calibrations','scripts/t80s_gen_download','scripts/t80s_process',
              'scripts/t80s_sort_calibrations', 'scripts/t80s_sort_night', 'scripts/t80s_sort_data']

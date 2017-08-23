445bdd3e352804dc366139f1f6d699f9e4a98825
William Schoenell <wschoenell@gmail.com>
2017-08-23 14:55:39 -0300

    missing s

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['astropy',
                       'colorlog',
                       'ccdproc>=0.3.3',
-                      'fit_reduce',
+                      'fits_reduce',
                       'pyyaml'],
     dependency_links=['https://github.com/tribeiro/fits_reduce/archive/master.zip'],
     scripts=['scripts/t80s_check_calibrations','scripts/t80s_gen_download','scripts/t80s_process',
